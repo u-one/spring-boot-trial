744eaaf342a73fecf21394e89e5b0b9b765bcc6a
u-one <backflip270bb@gmail.com>
2023-09-23 13:48:21 +0900

    first commit

---
+++ src/main/java/net/uoneweb/springboottrial/SpringBootTrialApplication.java
@@ -0,0 +1,13 @@
+package net.uoneweb.springboottrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/uoneweb/springboottrial/SpringBootTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.uoneweb.springboottrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
